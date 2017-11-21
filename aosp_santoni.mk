b192cb96b6acf92d4069e9bd4886c9b4ffccf03c
Tab Fitts <tfitts1212@gmail.com>
2017-11-21 13:03:56 -0700

    [santoni] Compatibility with fusion tree
    Change-Id: I1dd424f6f06bfa9ced27cddc97f55cec002844c2

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -29,7 +29,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
-$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := santoni
